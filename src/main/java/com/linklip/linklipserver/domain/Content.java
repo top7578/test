47cb4674a7250992ebecd17afdef84e0c619757f
junwoo <top7578@naver.com>
2022-07-23 11:08:10 +0900

    [GYJB-47] 기존코드 Google-Java-Format AOSP 적용

--- src/main/java/com/linklip/linklipserver/domain/Content.java
+++ src/main/java/com/linklip/linklipserver/domain/Content.java
@@ -3,10 +3,12 @@ package com.linklip.linklipserver.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
